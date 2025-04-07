e1bbda31ab1111b9b99f99b5635f7a1d513b8ed6
Chennojipriyanka <128712031+Chennojipriyanka@users.noreply.github.com>
2025-04-08 04:11:18 +0530

    ProductPage.java

--- src/test/java/TestData/ProductPage.java
+++ src/test/java/TestData/ProductPage.java
@@ -1,9 +1,44 @@
 package TestData;
 
+import java.time.Duration;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.JavascriptExecutor;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.support.ui.ExpectedConditions;
+import org.openqa.selenium.support.ui.WebDriverWait;
+
 public class ProductPage {
 
-	public static void main(String[] args) {
+	public static void main(String[] args) throws InterruptedException {
 		// TODO Auto-generated method stub
+		System.setProperty("webdriver.chrome.driver", "C:\\Users\\admin\\Downloads\\chromedriver_win32\\chromedriver.exe");
+
+		WebDriver driver = new ChromeDriver();
+
+		driver.manage().window().maximize();
+
+		WebDriverWait wait = new WebDriverWait(driver, Duration.ofSeconds(10));
+		
+		driver.get("https://www.amazon.in/");
+		 WebElement searchBox = wait.until(ExpectedConditions.visibilityOfElementLocated(By.id("twotabsearchtextbox")));
+	        searchBox.sendKeys("dress");
+
+	        WebElement searchButton = wait.until(ExpectedConditions.elementToBeClickable(By.id("nav-search-submit-button")));
+	        searchButton.click();
+
+	        
+	        WebElement firstProduct = wait.until(ExpectedConditions.elementToBeClickable(By.className("s-image")));
+	        firstProduct.click();
+
+	        WebElement addToCartBtn = wait.until(ExpectedConditions.visibilityOfElementLocated(By.id("add-to-cart-button")));
+	        JavascriptExecutor js = (JavascriptExecutor) driver;
+	        js.executeScript("arguments[0].scrollIntoView(true);", addToCartBtn);
+
+	      
+	        wait.until(ExpectedConditions.elementToBeClickable(addToCartBtn)).click();
 
 	}
 
