8f4c4cc291dfdfda08a4e08de9702f6c85c2a7ba
Chennojipriyanka <128712031+Chennojipriyanka@users.noreply.github.com>
2025-04-08 04:09:57 +0530

    HomePage.java

--- src/test/java/TestData/HomePage.java
+++ src/test/java/TestData/HomePage.java
@@ -1,9 +1,24 @@
 package TestData;
 
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+
 public class HomePage {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
+		
+		System.setProperty("webdriver.chrome.driver", "C:\\Users\\admin\\Downloads\\chromedriver_win32\\chromedriver.exe");
+
+		WebDriver driver = new ChromeDriver();
+
+		driver.manage().window().maximize();
+
+		driver.get("https://www.amazon.in/");
+
+		driver.findElement(By.id("twotabsearchtextbox")).sendKeys("dress");
+		driver.findElement(By.id("nav-search-submit-button")).submit();
 
 	}
 
