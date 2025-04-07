2b0e0d1968f19ba487929d694bddb03f4bf5798b
Chennojipriyanka <128712031+Chennojipriyanka@users.noreply.github.com>
2025-04-08 04:09:19 +0530

    LoginPage.java

--- src/test/java/TestData/LoginPage.java
+++ src/test/java/TestData/LoginPage.java
@@ -1,10 +1,33 @@
 package TestData;
 
+import org.openqa.selenium.By;
+import org.openqa.selenium.JavascriptExecutor;
+
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+
+
 public class LoginPage {
 
-	public static void main(String[] args) {
+	public static void main(String[] args) throws InterruptedException {
 		// TODO Auto-generated method stub
+		
+		System.setProperty("webdriver.chrome.driver", "C:\\Users\\admin\\Downloads\\chromedriver_win32\\chromedriver.exe");
 
+		WebDriver driver = new ChromeDriver();
+
+		driver.manage().window().maximize();
+
+		driver.get("https://www.amazon.in/");
+
+		driver.findElement(By.id("nav-link-accountList-nav-line-1")).click();
+		
+		driver.findElement(By.id("ap_email_login")).sendKeys("8379916604");
+		driver.findElement(By.className("a-button-input")).submit();
+
+		
+		driver.findElement(By.id("ap_password")).sendKeys("Priyanka@123");
+		driver.findElement(By.id("signInSubmit")).submit();
 	}
 
 }
